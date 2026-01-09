d6cb2affff3baa480073029c9d0e3b63d570dccf
Dhanashree Jadhav <dhanaa1611@gmail.com>
2026-01-09 23:59:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const app = express();
+const express = require('express');                      # ➡️ Imports the Express library ➡️ require is used to include external modules in Node.js.
+const app = express();                                   #  ➡️ Creates an Express application ➡️ app is now your web server.
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;                    # ➡️ Defines the port number First checks if AWS provides a port (process.env.PORT) If not, uses 3000 📌 This makes the app cloud-compatible.
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from AWS EC2!');
+  res.send('🚀 Hello from AWS EC2!');                      # ➡️ Creates a GET API route / → home page ,req → request from user ,res → response from server
 });
 
 app.get('/health', (req, res) => {
-  res.json({ status: 'healthy' });
+  res.json({ status: 'healthy' });                          # ➡️ Health check endpoint ➡️ Used to check if server is running correctly 
+                                                            # 📌 Useful in:Monitoring, Load balancers, DevOps setups
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);             # ➡️ Starts the server  PORT → 3000 , '0.0.0.0' → allows external traffic
 });
